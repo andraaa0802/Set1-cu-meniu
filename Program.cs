263bba597629704f727d4e8f9dfe48a7e1e6b250
andraaa0802 <92014452+andraaa0802@users.noreply.github.com>
2021-12-02 17:44:07 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,29 +9,31 @@ namespace _282
         static void Main(string[] args)
         {
             int optiune;
-            Console.WriteLine("Alegeti una din problemele de mai jos: "); Console.WriteLine();
-            Console.WriteLine("1. Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare."); Console.WriteLine();
-            Console.WriteLine("2. Rezolvati ecuatia de gradul 2 cu o necunoscuta: ax^2 + bx + c = 0, unde a, b si c sunt date de intrare.Tratati toate cazurile posibile."); Console.WriteLine();
-            Console.WriteLine("3. Determinati daca n se divide cu k, unde n si k sunt date de intrare."); Console.WriteLine();
-            Console.WriteLine("4. Detreminati daca un an y este an bisect."); Console.WriteLine();
-            Console.WriteLine("5. Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga."); Console.WriteLine();
-            Console.WriteLine("6. Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. "); Console.WriteLine();
-            Console.WriteLine("7. Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. "); Console.WriteLine();
-            Console.WriteLine("8. Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare."); Console.WriteLine();
-            Console.WriteLine("9. Afisati toti divizorii numarului n."); Console.WriteLine();
-            Console.WriteLine("10. Test de primalitate: determinati daca un numar n este prim."); Console.WriteLine();
-            Console.WriteLine("11. Afisati in ordine inversa cifrele unui numar n."); Console.WriteLine();
-            Console.WriteLine("12. Determinati cate numere integi divizibile cu n se afla in intervalul[a, b]."); Console.WriteLine();
-            Console.WriteLine("13. Determianti cati ani bisecti sunt intre anii y1 si y2."); Console.WriteLine();
-            Console.WriteLine("14. Determianti daca un numar n este palindrom. (un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321."); Console.WriteLine();
-            Console.WriteLine("15. Se dau 3 numere.Sa se afiseze in ordine crescatoare."); Console.WriteLine();
-            Console.WriteLine("16. Se dau 5 numere.Sa se afiseze in ordine crescatoare."); Console.WriteLine();
-            Console.WriteLine("17. Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere.Folositi algoritmul lui Euclid."); Console.WriteLine();
-            Console.WriteLine("18. Afisati descompunerea in factori primi ai unui numar n.De ex.pentru n = 1776 afisati 2 ^ 3 x 3 ^ 1 x 7 ^ 2."); Console.WriteLine();
-            Console.WriteLine("19. Determinati daca un numar e format doar cu 2 cifre care se pot repeta.De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt."); Console.WriteLine();
-            Console.WriteLine("20. Afisati fractia m / n in format zecimal, cu perioada intre paranteze(daca e cazul).Exemplu: 13 / 30 = 0.4(3)."); Console.WriteLine();
-            Console.WriteLine("21. Ghiciti un numar intre 1 si 1024 prin intrebari de forma \"numarul este mai mare sau egal decat x?\"."); Console.WriteLine();
+            
+            Console.WriteLine("1. Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare.\n"); 
+            Console.WriteLine("2. Rezolvati ecuatia de gradul 2 cu o necunoscuta: ax^2 + bx + c = 0, unde a, b si c sunt date de intrare.Tratati toate cazurile posibile.\n"); 
+            Console.WriteLine("3. Determinati daca n se divide cu k, unde n si k sunt date de intrare.\n"); 
+            Console.WriteLine("4. Detreminati daca un an y este an bisect.\n"); 
+            Console.WriteLine("5. Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga.\n"); 
+            Console.WriteLine("6. Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi.\n "); 
+            Console.WriteLine("7. Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. \n"); 
+            Console.WriteLine("8. Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare.\n"); 
+            Console.WriteLine("9. Afisati toti divizorii numarului n.\n"); 
+            Console.WriteLine("10. Test de primalitate: determinati daca un numar n este prim.\n"); 
+            Console.WriteLine("11. Afisati in ordine inversa cifrele unui numar n.\n"); 
+            Console.WriteLine("12. Determinati cate numere integi divizibile cu n se afla in intervalul[a, b].\n"); 
+            Console.WriteLine("13. Determianti cati ani bisecti sunt intre anii y1 si y2.\n"); 
+            Console.WriteLine("14. Determianti daca un numar n este palindrom. (un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321).\n"); 
+            Console.WriteLine("15. Se dau 3 numere.Sa se afiseze in ordine crescatoare.\n"); 
+            Console.WriteLine("16. Se dau 5 numere.Sa se afiseze in ordine crescatoare.\n"); 
+            Console.WriteLine("17. Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere.Folositi algoritmul lui Euclid.\n"); 
+            Console.WriteLine("18. Afisati descompunerea in factori primi ai unui numar n.De ex.pentru n = 1776 afisati 2 ^ 3 x 3 ^ 1 x 7 ^ 2.\n");
+            Console.WriteLine("19. Determinati daca un numar e format doar cu 2 cifre care se pot repeta.De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt.\n"); 
+            Console.WriteLine("20. Afisati fractia m / n in format zecimal, cu perioada intre paranteze(daca e cazul).Exemplu: 13 / 30 = 0.4(3).\n"); 
+            Console.WriteLine("21. Ghiciti un numar intre 1 si 1024 prin intrebari de forma \"numarul este mai mare sau egal decat x?\".\n"); 
 
+            Console.WriteLine("Alegeti una din problemele de mai sus: \n"); 
+            
             optiune = int.Parse(Console.ReadLine());
             switch (optiune)
             {
@@ -100,27 +102,7 @@ namespace _282
                     break;
 
             }
-            //P1();
-            //P2();
-            //P3();
-            //P4();
-            //P5();
-            //P6();
-            //P7();
-            //P8();
-            //P9();
-            //P10();
-            //P11();
-            //P12();
-            //P13();
-            //P14();
-            //P15();
-            //P16();
-            //P17();
-            //P18();
-            //P19();
-            //P20();
-            //P21();
+           
         }
 
         /// <summary>
